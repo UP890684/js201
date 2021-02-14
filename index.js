ac53acfccb8c675f94dad66c4b2c917064da2f17
Max Watson <62099599+UP890684@users.noreply.github.com>
2021-02-14 21:14:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -44,13 +44,125 @@ function checkObject(obj)
 
 function checkObjectInside(obj)
 {
-  obj.data
+  if (obj.data) obj.data.checked = true;
+}
+
+function arraySet(arr, i, n)
+{
+  if (i < arr.length && Number.isInteger(i))
   {
-    checked: undefined
-  };
-  if (obj.data = !!undefined)
+    arr[i] = n;
+  }
+}
+
+function addAll(arr)
+{
+  let arrSum = 0;
+
+  for (let i = 0; i < arr.length; i++)
   {
-    data.checked = true;
-    return obj.data;
-  } 
+    arrSum += arr[i]
+  }
+
+  return arrSum;
+
+  /*
+  let i = 0;
+  let x = 0;
+
+  while (i < arr.length)
+  {
+    x += arr[i];
+    i += 1;
+  }
+  return x;
+  */
+}
+
+function larger(a, b)
+{
+  if (a > b)
+  {
+    return a;
+  }
+  else
+  {
+    return b;
+  }
+}
+
+function largest(arr)
+{
+  let largest = null;
+
+  for (let i = 0; i < arr.length; i++)
+  {
+    if (arr[i] > largest)
+    {
+      largest = arr[i];
+    }
+  }
+  return largest;
+}
+
+function compare(a, b)
+{
+  let check = true;
+
+  if (a.length === b.length)
+  {
+    for (let i = 0; i < a.length; i++)
+    {
+      if (a[i] === b[i])
+      {
+        check = true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
+  else
+  {
+    return false;
+  }
+  return check;
+}
+
+function addToAll(arr, n)
+{
+  let arr2;
+
+  for (let i = 0; i < arr.length; i++)
+  {
+    let number = arr[i] + n;
+    arr[i] = number;
+  }
+  return arr;
+}
+
+function rememberThis()
+{
+
+}
+
+function nArray(n)
+{
+
+}
+
+function addAllOpt(arr)
+{
+
+}
+
+function divisors(arr, div)
+{
+
+}
+
+function multiples(n, m)
+{
+  
 }
